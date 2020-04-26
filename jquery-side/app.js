e0ad4988cb4c2e9bd9a9e52efdf7f3acff4c372e
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-04-25 22:49:45 -0500

    finished lab

--- jquery-side/app.js
+++ jquery-side/app.js
@@ -34,3 +34,41 @@ square.mouseout(function () {
 })
 
 //#4
+
+let pTag = $('[id="pTag"]')
+pTag.click(function () {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    pTag.css('color', color);
+
+})
+
+//#5
+let nameDiv = $('[id=nameDiv]');
+let nameBtn = $('[id=nameBtn]');
+nameBtn.click(function () {
+    Swal.fire({
+        title: 'Michelle',
+        text: 'Cause I\'m so magical!',
+        imageUrl: 'https://i.pinimg.com/564x/a1/ed/22/a1ed229fc9d37662846b81c641d2e860.jpg',
+        imageWidth: 250,
+        imageHeight: 450,
+        imageAlt: 'Custom image',
+    })
+    let name = $('<span>Michelle</span>')
+    $('[id=nameDiv]').append(name)
+}
+)
+
+//#6
+let array = ['Rey', 'BB-8', 'Pider-man', 'Luke', 'Chewy', 'Nightcrawler', 'Mickey Mouse', 'Rocket', 'Groot', 'Jesus'];
+$('[id=squadBtn]').click(function () {
+    for (i = 0; i < array.length; i++) {
+        var name = array[i]
+        var list = $('<li></li>')
+        $(list).append(name)
+        $('[id=squadGoals]').append(list)
+    }
+})
